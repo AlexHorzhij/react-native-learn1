1406d001c7822d5e91859590c62559d7b02b62af
AlexHorzhij <97761791+AlexHorzhij@users.noreply.github.com>
2023-03-12 23:16:35 +0200

    add login and register screens

--- App.js
+++ App.js
@@ -1,36 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, ImageBackground } from 'react-native';
 import { LoginScreen } from './Screens/LoginScreen';
+import { RegistrationScreen } from './Screens/RegistrationScreen';
+import { useFonts } from 'expo-font';
+
+import { StyleSheet, View } from 'react-native';
+
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'Roboto-Regular': require('./assets/fonts/Roboto-Regular.ttf'),
+    'Roboto-Regular': require('./assets/fonts/Roboto-Medium.ttf'),
+    'Roboto-Regular': require('./assets/fonts/Roboto-Bold.ttf'),
+  });
+
   return (
-    <ImageBackground
-      style={styles.background}
-      resizeMode={'cover'}
-      source={require('./assets/images/backgroundFoto.jpg')}
-    >
-      <View style={styles.container}>
-        <LoginScreen />
-        <StatusBar style="auto" />
-      </View>
-    </ImageBackground>
+    <View style={styles.container}>
+      <LoginScreen />
+      {/* <RegistrationScreen /> */}
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     // backgroundColor: '#fff',
-//     // alignItems: 'center',
-//     // justifyContent: 'center',
-//   },
-// });
 const styles = StyleSheet.create({
-  background: {
-    flex: 1,
-    justifyContent: 'center',
-  },
   container: {
     flex: 1,
-    justifyContent: 'flex-end',
   },
 });
--- Screens/LoginScreen.js
+++ Screens/LoginScreen.js
@@ -1,45 +1,120 @@
+import { useState, useEffect } from 'react';
+
 import {
   StyleSheet,
   TextInput,
+  ImageBackground,
   View,
   Text,
-  Button,
   TouchableOpacity,
+  TouchableWithoutFeedback,
+  KeyboardAvoidingView,
+  Keyboard,
 } from 'react-native';
 
+const initialState = {
+  login: '',
+  password: '',
+};
+
 export const LoginScreen = () => {
+  const [state, setState] = useState(initialState);
+  const [isOpenKeyboard, setIsOpenKeyboard] = useState(false);
+
+  useEffect(() => {
+    const showSubscription = Keyboard.addListener('keyboardDidShow', () => {
+      setIsOpenKeyboard(true);
+    });
+    const hideSubscription = Keyboard.addListener('keyboardDidHide', () => {
+      setIsOpenKeyboard(false);
+    });
+
+    return () => {
+      showSubscription.remove();
+      hideSubscription.remove();
+    };
+  }, []);
+
+  const keyboardVisibleHandler = flag => {
+    setIsOpenKeyboard(flag);
+    if (!flag) {
+      Keyboard.dismiss();
+    }
+  };
+
+  const submitForm = () => {
+    keyboardVisibleHandler(false);
+  };
+
   return (
-    <View style={styles.loginContainer}>
-      <Text style={styles.title}>Login</Text>
-      <View style={styles.formContainer}>
-        <TextInput
-          style={{ ...styles.input, marginBottom: 16 }}
-          placeholder={'Login'}
-        />
-        <TextInput
-          style={{ ...styles.input, marginBottom: 43 }}
-          placeholder={'Password'}
-        />
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.buttonText}>Sign in</Text>
-        </TouchableOpacity>
-      </View>
-    </View>
+    <TouchableWithoutFeedback onPress={() => keyboardVisibleHandler(false)}>
+      <ImageBackground
+        style={styles.background}
+        resizeMode={'cover'}
+        source={require('../assets/images/backgroundFoto.jpg')}
+      >
+        <View
+          style={{
+            ...styles.loginContainer,
+            height: isOpenKeyboard ? 248 : 485,
+          }}
+        >
+          <KeyboardAvoidingView
+            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+          >
+            <Text style={styles.title}>Login</Text>
+            <View style={styles.formContainer}>
+              <TextInput
+                style={{ ...styles.input, marginBottom: 16 }}
+                placeholder={'Login'}
+                value={state.login}
+                onChangeText={value =>
+                  setState(prev => ({ ...prev, login: value }))
+                }
+                onFocus={() => keyboardVisibleHandler(true)}
+              />
+              <TextInput
+                style={{ ...styles.input, marginBottom: 43 }}
+                placeholder={'Password'}
+                value={state.password}
+                onChangeText={value =>
+                  setState(prev => ({ ...prev, password: value }))
+                }
+                onFocus={() => keyboardVisibleHandler(true)}
+              />
+              <TouchableOpacity style={styles.button} onPress={submitForm}>
+                <Text style={styles.buttonText}>Sign in</Text>
+              </TouchableOpacity>
+              <Text style={styles.linkText}>
+                You don't have account? Sing up!
+              </Text>
+            </View>
+          </KeyboardAvoidingView>
+        </View>
+      </ImageBackground>
+    </TouchableWithoutFeedback>
   );
 };
 
 const styles = StyleSheet.create({
+  // keyboard: {},
   title: {
+    fontFamily: 'Roboto',
     alignSelf: 'center',
     fontWeight: '500',
     fontSize: 30,
     marginBottom: 32,
   },
+  background: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end',
+  },
   loginContainer: {
+    width: '100%',
     paddingTop: 32,
     borderTopLeftRadius: 25,
     borderTopRightRadius: 25,
-    height: 489,
     backgroundColor: '#FFFFFF',
   },
   formContainer: {
@@ -66,6 +141,11 @@ const styles = StyleSheet.create({
   buttonText: {
     textTransform: 'uppercase',
     color: '#fff',
+    fontSize: 16,
+  },
+  linkText: {
+    alignSelf: 'center',
+    color: '#1B4371',
     fontSize: 16,
   },
 });
